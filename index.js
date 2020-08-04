1e9ba194a4414b2c44776b07658b79632d365ecc
Albert Margaryan <albertmargaryan2004@gmail.com>
2020-08-04 17:31:22 +0400

    Added login page and validator

---
+++ index.js
@@ -0,0 +1,9 @@
+function verifylogin() {
+	let user = document.getElementById('user').value
+	let pass = document.getElementById('pass').value
+
+	let verify = user.length > 5 && pass.length > 5
+	if(verify == true) {
+		console.log('lox')
+	}
+}
